3ca49e6a0251ab721b4b67de77ece51cf71b71e1
priyadharshan2003 <priyan2003chandru@gmail.com>
2024-10-06 22:13:16 +0530

    update on config files

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar.tsx'
-import Footer from './components/Footer.tsx'
-import Home from './pages/Home.tsx'
-import ContractGenerator from './pages/ContractGenerator.tsx'
-import ContractDetail from './pages/ContractDetail.tsx'
-import ComplianceMonitoring from './pages/ComplianceMonitoring.tsx'
-import Subscription from './pages/Subscription.tsx'
-import OrderHistory from './pages/OrderHistory.tsx'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import ContractGenerator from './pages/ContractGenerator'
+import ContractDetail from './pages/ContractDetail'
+import ComplianceMonitoring from './pages/ComplianceMonitoring'
+import Subscription from './pages/Subscription'
+import OrderHistory from './pages/OrderHistory'
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/generate-contract" element={<ContractGenerator />} />
-            <Route path="/contract/:id" element={<ContractDetail />} />
-            <Route path="/compliance-monitoring" element={<ComplianceMonitoring />} />
-            <Route path="/subscription" element={<Subscription />} />
-            <Route path="/order-history" element={<OrderHistory />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/generate-contract" element={<ContractGenerator />} />
+          <Route path="/contract/:id" element={<ContractDetail />} />
+          <Route path="/compliance-monitoring" element={<ComplianceMonitoring />} />
+          <Route path="/subscription" element={<Subscription />} />
+          <Route path="/order-history" element={<OrderHistory />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
